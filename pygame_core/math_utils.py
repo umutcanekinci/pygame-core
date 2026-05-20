d4c71ce5a335e19ae39c28dffd255d1a91be0d60
Umutcan Ekinci <umutcannekinci@gmail.com>
2026-05-21 01:18:38 +0300

    Add math_utils: Vector2 distance and angle helpers
    Lifted from tower-defense's util/math.py; useful for any project doing
    rotation or proximity work. Renamed from 'math' to avoid stdlib confusion.

---
+++ pygame_core/math_utils.py
@@ -0,0 +1,15 @@
+from math import atan2, degrees
+
+from pygame import Vector2
+
+
+def distance(a: Vector2, b: Vector2) -> float:
+    return ((a.x - b.x) ** 2 + (a.y - b.y) ** 2) ** 0.5
+
+
+def angle_between_points(origin_point: Vector2, target_point: Vector2) -> float:
+    return angle_between_delta(target_point - origin_point)
+
+
+def angle_between_delta(delta: Vector2) -> float:
+    return degrees(atan2(delta.y, delta.x))
